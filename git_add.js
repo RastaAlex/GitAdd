df97642f0e7bb2b969de9a8f27c451a63ff4593f
RastaAlex <Rastaalex86@gmail.com>
2020-05-07 17:08:50 +0300

    New commit

---
+++ git_add.js
@@ -0,0 +1,18 @@
+const git = require('isomorphic-git');
+const fs = require('fs');
+const dir = process.cwd();
+
+async function main () {
+    await git.add({fs, dir, filepath: 'git_add.js'});
+    await git.commit({ fs, dir, message: 'New commit',author: {name: 'RastaAlex', email: 'Rastaalex86@gmail.com' }});
+    await git.push({
+        fs,
+        http,
+        dir: '/tutorial',
+        remote: 'origin',
+        ref: 'master',
+    });
+    
+}
+main().catch(err => console.log(err.message));
+
