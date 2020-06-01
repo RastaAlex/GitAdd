ef03a92668c52cf1d3786b168811fa4252b8c363
RastaAlex <RastaAlex86@gmail.com>
2020-06-01 19:06:22 +0300

    New commit

--- gitadd.js
+++ gitadd.js
@@ -1,33 +1,12 @@
-const git = require('isomorphic-git');
 const fs = require('fs');
-const http = require('isomorphic-git/http/node');
-const dir = process.cwd();
+const exec = require('child_process');
 
-async function main () {
-    const add = await git.add({fs, dir, filepath: '.'});
-    const newcommit  = await git.commit({
-        fs,
-        dir: './',
-        author: {
-            name: 'RastaAlex',
-            email: 'Rastaalex86@gmail.com'
-        },
-        ref: 'master',
-        message: 'Added new changes to file'
-      })
-      console.log(newcommit);
- 
-     const push = await git.push({
-        fs,
-        http,
-        dir: './',
-        url: 'https://github.com/RastaAlex/GitAdd',
-        remote: 'origin',
-        ref: 'master',
-        onAuth: () => ({ username: process.env.GITHUB_TOKEN}),
-        })
-      
-       console.log(push);  
-      
-}
-main().catch(err => console.log(err.message));
+fs.watch('./', (event, filename) => {
+    if (event === 'change' && filename === 'gitadd.js') {
+        console.log(`some changes in ${filename}`);
+        exec.execSync(`git add ${filename}`);
+        exec.execSync('git commit -m "New commit"');
+    }
+});
+
+const a ='abcdfg';
