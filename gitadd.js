ffa9be3b3ddb15e13823e2f88d3211e18274022b
RastaAlex <RastaAlex86@gmail.com>
2020-06-01 21:59:17 +0300

    New commit

---
+++ gitadd.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const exec = require('child_process');
+const tryCatch = require('try-catch');
+
+fs.watch('./', (event, filename) => {
+    if (event === 'change' && filename === 'gitadd.js') {
+        console.log(`some changes in ${filename}`);
+        try{
+        exec.execSync(`git add ${filename}`);
+        } catch(err) {
+            console.log(err.message);
+        }
+        try{
+        exec.execSync('git commit -m "New commit"');
+        } catch(err) {
+            console.log(err.message);
+        }
+    }
+});
+
+const a ='abcdffffffgffggggg';
