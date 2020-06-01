a0acd416c94f1d27c6f11a4496d366af302645d1
RastaAlex <RastaAlex86@gmail.com>
2020-06-01 19:58:48 +0300

    New commit

---
+++ gitadd.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const exec = require('child_process');
+
+fs.watch('./', (event, filename) => {
+    if (event === 'change' && filename === 'gitadd.js') {
+        console.log(`some changes in ${filename}`);
+        exec.execSync(`git add ${filename}`);
+        exec.execSync('git commit -m "New commit"');
+    }
+});
+
+const a ='abcdfffffff';
