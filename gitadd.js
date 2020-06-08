a5547ae1bf9c4026e41c885908f44efa04c8c208
RastaAlex <RastaAlex86@gmail.com>
2020-06-08 18:36:42 +0300

    New commit

--- gitadd.js
+++ gitadd.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
 const exec = require('child_process');
 const tryCatch = require('try-catch');
-const git = require('simple-git/promise');
 
-const a ='abcdffffff';
+fs.watch('./', (event, filename) => {
+    if (event === 'change' && filename === 'gitadd.js') {
+        console.log(`some changes in ${filename}`);
+        const[error, result] = tryCatch(exec.execSync,`git add ${filename}`);
+        if(error)
+            console.error(error.message);
+       const[err, res] = tryCatch(exec.execSync,'git commit -m "New commit"');
+         if(err)
+            console.error(err.message);
+      }
+});
 
- async function main() {
-    git().add('./*');
-    git().commit('new commit');
-    git().push('origin', 'master');
-     
-         
- }
-
- main().catch((err) => console.log(err.message));
-
-
+const abc = 'abc';
